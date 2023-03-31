429c3779c68ca308666712cddb3323dd9b9b2276
kshetritej <furiousdtej@gmail.com>
2023-03-31 20:50:26 +0545

    create rainbow color function

--- main.js
+++ main.js
@@ -16,16 +16,49 @@ for (let i = 0; i <= 25; i++) {
     sketchpadScreen.appendChild(squares);
     squares.addEventListener("mouseover",()=>{
       squares.style.backgroundColor = "yellow";
-    });
-
-    clearBtn.addEventListener("click",()=>{
-      squares.style.backgroundColor = "black";
-    });
-
-    eraserBtn.addEventListener("click",()=>{
-      squares.addEventListener("mouseover",()=>{
-        squares.style.backgroundColor = "black";
-      });
-    });
+    }); 
   }
 }
+
+// newGrid buttons re-enable the same action as of first
+// have to enable user to create desired pixel size
+newGridBtn.addEventListener("click",()=>{
+  const shapes = document.querySelectorAll(".pixel-square");
+  shapes.forEach(shape =>{
+    shape.addEventListener("mouseover",()=>{
+      shape.style.backgroundColor = "yellow";
+    });
+  });
+});
+
+// clears whole sketchpad
+clearBtn.addEventListener("click",()=>{
+  const pixels = document.querySelectorAll(".pixel-square");
+  pixels.forEach(pixel => {
+    pixel.style.backgroundColor = "black";
+  });
+  pixels.addEventListener("mouseover",()=>{
+    pixels.style.backgroundColor = "yellow";
+  })
+});
+
+//erase colored pixel on hover
+eraserBtn.addEventListener("click",()=>{
+  const pixels = document.querySelectorAll(".pixel-square");
+  pixels.forEach(pixel =>{
+    pixel.addEventListener("mouseover",()=>{
+      pixel.style.backgroundColor = "black";
+    });
+  });
+});
+
+// Rainbow button , creates random color for every pixel
+rainbowBtn.addEventListener("click",()=>{
+  boxes = document.querySelectorAll(".pixel-square");
+  boxes.forEach(box=>{
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    box.addEventListener("mouseover",()=>{
+      box.style.backgroundColor = "#"+ randomColor;
+    });
+  });
+});
